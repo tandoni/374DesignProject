3e5844661c938e88d9e3fb4a47c351e6b75f215f
Max Morgan <maxmorgan94@gmail.com>
2016-01-12 17:14:26 -0500

    added the new class that will produce the SD output

---
+++ src/TransferToUML/impl/SDOutputStream.java
@@ -0,0 +1,39 @@
+package TransferToUML.impl;
+
+import TransferToUML.api.IClass;
+import TransferToUML.api.IModel;
+import TransferToUML.visitor.VisitorAdapter;
+
+public class SDOutputStream extends VisitorAdapter {
+
+	@Override
+	public void preVisit(IClass c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IClass c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void postVisit(IClass c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IModel m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitSperator() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
