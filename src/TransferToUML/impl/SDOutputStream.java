5fb8afa5d92247651319a9c2f136153c7ba686ae
Max Morgan <maxmorgan94@gmail.com>
2016-01-12 18:49:59 -0500

    More changes to SDOutputStream

--- src/TransferToUML/impl/SDOutputStream.java
+++ src/TransferToUML/impl/SDOutputStream.java
@@ -1,10 +1,27 @@
 package TransferToUML.impl;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import TransferToUML.api.IClass;
 import TransferToUML.api.IModel;
 import TransferToUML.visitor.VisitorAdapter;
 
 public class SDOutputStream extends VisitorAdapter {
+	private final OutputStream out;
+
+	public SDOutputStream(OutputStream out) throws IOException {
+		super();
+		this.out = out;
+	}
+
+	private void write(String s) {
+		try {
+			this.out.write(s.getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@Override
 	public void preVisit(IClass c) {
