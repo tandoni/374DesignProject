9d9739a0eb8a4737fd574a0f2c8f9625eeff6b43
CHEN, Ruying <chenr@rose-hukman.edu>
2016-01-28 15:09:50 -0500

    SignelTestsForM1&2

---
+++ testClasses/ClassWIthInterfaceAndSuperClass.java
@@ -0,0 +1,18 @@
+
+public class ClassWIthInterfaceAndSuperClass extends EmptyClass implements EmptyInterface{
+	
+	private String str;
+	
+	public ClassWIthInterfaceAndSuperClass(){
+	}
+	
+	public void setStr(String s){
+		this.str = s;
+	}
+	
+	public String gteStr(){
+		return this.str;
+	}
+
+}
+
---
+++ testClasses/ClassWithUses.java
@@ -0,0 +1,11 @@
+
+public class ClassWithUses {
+
+	@SuppressWarnings("unused")
+	private EmptyClass clazz = new EmptyClass();
+	
+	public ClassWithUses(EmptyClass clazz) {
+		this.clazz = clazz;
+	}
+
+}
