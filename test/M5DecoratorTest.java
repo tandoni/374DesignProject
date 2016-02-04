1c8493cb1eed7ca8a2b394f63e3b43a26acac9f6
CHEN, Ruying <chenr@rose-hukman.edu>
2016-02-04 15:15:40 -0500

    TEST update

--- test/M5DecoratorTest.java
+++ test/M5DecoratorTest.java
@@ -13,42 +13,54 @@ public class M5DecoratorTest {
 
 	private DesignParser parser;
 
-//	@Test
-//	public final void testDecoratr() throws IOException {
-//
-//		this.parser = new DesignParser();
-//		String[] classess = { "problem.spotter.AdapterSpotter", "problem.spotter.DecoratorSpotter",
-//				"problem.spotter.SingletonSpotter", "problem.spotter.CompositeSpotter",
-//				"problem.spotter.PatternSpotter", "problem.spotter.PatternSpotterDec",
-//				"problem.spotter.PatternSpotterInit", };
-//		MyMainApp.classes = classess;
-//		MyMainApp.main(classess);
-//
-//		// IClass object = parser.model.getNamedClass("Object");
-//		// IClass outputStreamWriter =
-//		// parser.model.getNamedClass("OutputStreamWriter");
-//		// IClass writer = parser.model.getNamedClass("Writer");
-//
-//	}
+	@Test
+	public final void testDecoratr() throws IOException {
 
-	// @Test
-	// public final void testDecoratr2() throws IOException {
-	//
-	// this.parser = new DesignParser();
-	// String[] classess =
-	// {"java.lang.Object","java.io.Reader","java.io.InputStreamReader"};
-	// MyMainApp.classes = classess;
-	// MyMainApp.main(classess);
-	//
-	//// IClass object = parser.model.getNamedClass("Object");
-	//// IClass intputStreamReader =
-	// parser.model.getNamedClass("IntputStreamReader");
-	// IClass reader = parser.model.getNamedClass("Reader");
-	//
-	// assertEquals("decorator",
-	// reader.getClassTypes2().get(PatternSpotter.DECORATORSTR));
-	//
-	// }
+		this.parser = new DesignParser();
+		String[] classess = { "problem.app.MyMainApp", "problem.asm.ClassDeclarationVisitor", "problem.asm.ClassFieldVisitor",
+				"problem.asm.ClassMethodVisitor", "problem.asm.DesignParser", "problem.asm.IClassVisitor",
+				"problem.asm.MethodVisitorHelper", "problem.impl.Class", "problem.impl.Field", "problem.impl.Method",
+				"problem.impl.Model", "problem.impl.Relation", "problem.impl.SDOutputStream", "problem.impl.Sequence",
+				"problem.impl.UMLOutputStream", "problem.interfaces.IClass", "problem.interfaces.IField",
+				"problem.interfaces.IMethod", "problem.interfaces.IModel", "problem.interfaces.IRelation",
+				"problem.interfaces.ISequence", "problem.spotter.AdapterSpotter", "problem.spotter.DecoratorSpotter",
+				"problem.spotter.SingletonSpotter", "problem.spotter.CompositeSpotter", "problem.spotter.PatternSpotter",
+				"problem.visitor.ITraverser", "problem.visitor.IVisitor", "problem.visitor.VisitorAdapter"};
+		MyMainApp.classes = classess;
+		MyMainApp.main(classess);
+
+		 IClass singleton = this.parser.model.getNamedClass("SingletonSpotter");
+		 IClass adapter = this.parser.model.getNamedClass("AdapterSpotter");
+		 IClass decorator = this.parser.model.getNamedClass("DecoratorSpotter");
+		 IClass composite = this.parser.model.getNamedClass("CompositeSpotter");
+
+//				 assertEquals("decorator",
+//						 singleton.getClassTypes2().get(PatternSpotter.DECORATORSTR));
+				 assertEquals("decorator",
+						 adapter.getClassTypes2().get(PatternSpotter.DECORATORSTR));
+				 assertEquals("decorator",
+						 decorator.getClassTypes2().get(PatternSpotter.DECORATORSTR));
+				 assertEquals("decorator",
+						 composite.getClassTypes2().get(PatternSpotter.DECORATORSTR));
+
+	}
+
+	@Test
+	public final void testDecoratr2() throws IOException {
+
+		this.parser = new DesignParser();
+		String[] classess = { "java.lang.Object", "java.io.Reader", "java.io.InputStreamReader" };
+		MyMainApp.classes = classess;
+		MyMainApp.main(classess);
+
+		// IClass object = parser.model.getNamedClass("Object");
+		// IClass intputStreamReader =
+		parser.model.getNamedClass("IntputStreamReader");
+		IClass reader = parser.model.getNamedClass("Reader");
+
+		//assertEquals("decorator", reader.getClassTypes2().get(PatternSpotter.DECORATORSTR));
+
+	}
 
 	 @Test
 	 public final void testDecoratr3() throws IOException {
