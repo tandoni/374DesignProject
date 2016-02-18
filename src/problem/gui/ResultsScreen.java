51354364459580b17f2f3c89bf275b6299358fc0
shadowalker <ishank.tandon@hotmail.com>
2016-02-18 17:52:26 -0500

    unwanted code

--- src/problem/gui/ResultsScreen.java
+++ src/problem/gui/ResultsScreen.java
@@ -59,16 +59,16 @@ public class ResultsScreen extends JFrame {
 		}
 
 		// debug exec
-		InputStream is = p.getErrorStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		String line;
-
-		System.out.printf("\n\n\n\n");
-
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
-		}
+//		InputStream is = p.getErrorStream();
+//		InputStreamReader isr = new InputStreamReader(is);
+//		BufferedReader br = new BufferedReader(isr);
+//		String line;
+//
+//		System.out.printf("\n\n\n\n");
+//
+//		while ((line = br.readLine()) != null) {
+//			System.out.println(line);
+//		}
 		// end here
 
 		super.setTitle("UMLLAMA");
@@ -106,89 +106,6 @@ public class ResultsScreen extends JFrame {
 			this.setFont(new Font("Helvetica", Font.BOLD, 16));
 			this.setPreferredSize(new Dimension(250, 5));
 			this.append("selection logic goes here");
-		}
-	}
-
-	class ScrollablePicture extends JLabel implements Scrollable, MouseMotionListener {
-
-		private int maxUnitIncrement = 1;
-		private boolean missingPicture = false;
-
-		public ScrollablePicture(ImageIcon i, int m) {
-			super(i);
-			if (i == null) {
-				missingPicture = true;
-				setText("No picture found.");
-				setHorizontalAlignment(CENTER);
-				setOpaque(true);
-				setBackground(Color.white);
-			}
-			maxUnitIncrement = m;
-
-			// Let the user scroll by dragging to outside the window.
-			setAutoscrolls(true); // enable synthetic drag events
-			addMouseMotionListener(this); // handle mouse drags
-		}
-
-		// Methods required by the MouseMotionListener interface:
-		public void mouseMoved(MouseEvent e) {
-		}
-
-		public void mouseDragged(MouseEvent e) {
-			// The user is dragging us, so scroll!
-			Rectangle r = new Rectangle(e.getX(), e.getY(), 1, 1);
-			scrollRectToVisible(r);
-		}
-
-		public Dimension getPreferredSize() {
-			if (missingPicture) {
-				return new Dimension(320, 480);
-			} else {
-				return super.getPreferredSize();
-			}
-		}
-
-		public Dimension getPreferredScrollableViewportSize() {
-			return getPreferredSize();
-		}
-
-		public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
-			// Get the current position.
-			int currentPosition = 0;
-			if (orientation == SwingConstants.HORIZONTAL) {
-				currentPosition = visibleRect.x;
-			} else {
-				currentPosition = visibleRect.y;
-			}
-
-			// Return the number of pixels between currentPosition
-			// and the nearest tick mark in the indicated direction.
-			if (direction < 0) {
-				int newPosition = currentPosition - (currentPosition / maxUnitIncrement) * maxUnitIncrement;
-				return (newPosition == 0) ? maxUnitIncrement : newPosition;
-			} else {
-				return ((currentPosition / maxUnitIncrement) + 1) * maxUnitIncrement - currentPosition;
-			}
-		}
-
-		public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
-			if (orientation == SwingConstants.HORIZONTAL) {
-				return visibleRect.width - maxUnitIncrement;
-			} else {
-				return visibleRect.height - maxUnitIncrement;
-			}
-		}
-
-		public boolean getScrollableTracksViewportWidth() {
-			return false;
-		}
-
-		public boolean getScrollableTracksViewportHeight() {
-			return false;
-		}
-
-		public void setMaxUnitIncrement(int pixels) {
-			maxUnitIncrement = pixels;
 		}
 	}
 
