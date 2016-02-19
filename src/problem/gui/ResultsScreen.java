28b9fdd293360de06f30c570fe47e411b5bbc678
shadowalker <ishank.tandon@hotmail.com>
2016-02-18 19:06:12 -0500

    Fixed Max's code.

--- src/problem/gui/ResultsScreen.java
+++ src/problem/gui/ResultsScreen.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.io.IOException;
@@ -17,6 +18,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
@@ -32,7 +34,7 @@ public class ResultsScreen extends JFrame {
 	private static final int HEIGHT = 1080;
 	Container content;
 	MyMainApp app;
-	private JPanel response;
+	private Response response;
 
 	public ResultsScreen(MyMainApp app) throws IOException {
 		this.app = app;
@@ -68,11 +70,10 @@ public class ResultsScreen extends JFrame {
 		// }
 		// end here
 
-		super.setTitle("UMLLAMA");
+		super.setTitle("Python4Lyfe");
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.panel = new Panel();
-		this.response = new JPanel(new GridLayout(0, 1));
-
+		this.response = new Response();
 		this.setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.content = getContentPane();
@@ -82,7 +83,6 @@ public class ResultsScreen extends JFrame {
 		border.setTitleJustification(TitledBorder.LEFT);
 		this.panel.setBorder(border);
 		this.content.add(this.panel, BorderLayout.CENTER);
-		this.generateRespone();
 		this.content.add(this.response, BorderLayout.WEST);
 
 		setVisible(true);
@@ -94,44 +94,54 @@ public class ResultsScreen extends JFrame {
 	 * 
 	 * @author morganml
 	 */
-	private void generateRespone() {
-		// public Response(MyMainApp app) {
-		// this.setEditable(false);
-		TitledBorder border = BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), "select");
-		// border.setTitleJustification(TitledBorder.LEFT);
-		// this.setBorder(border);
-		// this.setFont(new Font("Helvetica", Font.BOLD, 16));
-		// this.setPreferredSize(new Dimension(250, 5));
-		// this.append("selection logic goes here");
-		JPanel panel = new JPanel(new GridLayout(0, 1));
-		HashMap<String, Boolean> pMap = MyMainApp.getParser().model.getContainsPatternMap();
-		// This list is to be checked against the hashMap of patterns from Model
-		// to see if a given pattern exists in the UML
-		Collection<String> patternList = MyMainApp.getParser().model.getPatternNames();
-		// Iterate through the list of strings, and check the pattern map in
-		// Model to see if that pattern is respresented in Model. If it is, then
-		// find all classes with that pattern, and display their checkboxes
-		// below a checkbox that is the name of the pattern
-		for (String pattern : patternList) {
-			// If this is true, then we know this pattern is represented in
-			// the that patterns map
-			if (pMap.get(pattern)) {
-				JPanel patternPanel = new JPanel();
-				patternPanel.setLayout(new GridLayout(0, 1));
+	class Response extends JPanel {
 
-				JCheckBox check = new JCheckBox(pattern);
-				patternPanel.add(check);
-				for (IClass c : MyMainApp.getParser().model.getClasses()) {
-					if (c.getClassTypes2().containsKey(pattern)) {
-						check = new JCheckBox(c.getName());
-						check.setBorder(new EmptyBorder(0, 20, 0, 0));
-						patternPanel.add(check);
-					}
-				}
-				this.response.add(patternPanel);
-			}
+		public Response() {
+			TitledBorder border = BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), "select");
+			border.setTitleJustification(TitledBorder.LEFT);
+			this.setBorder(border);
+			this.setFont(new Font("Helvetica", Font.BOLD, 16));
+			this.setPreferredSize(new Dimension(250, 5));
+			
+			generateResponse();
 		}
 
+		private void generateResponse() {
+			HashMap<String, Boolean> pMap = MyMainApp.getParser().model.getContainsPatternMap();
+			// This list is to be checked against the hashMap of patterns from
+			// Model
+			// to see if a given pattern exists in the UML
+			Collection<String> patternList = MyMainApp.getParser().model.getPatternNames();
+			// Iterate through the list of strings, and check the pattern map in
+			// Model to see if that pattern is respresented in Model. If it is,
+			// then
+			// find all classes with that pattern, and display their checkboxes
+			// below a checkbox that is the name of the pattern
+			for (String pattern : patternList) {
+				// If this is true, then we know this pattern is represented in
+				// the that patterns map
+				if (pMap.get(pattern)) {
+					JPanel patternPanel = new JPanel();
+					patternPanel.setLayout(new GridLayout(0, 1));
+
+					JCheckBox check = new JCheckBox(pattern);
+					patternPanel.add(check);
+					for (IClass c : MyMainApp.getParser().model.getClasses()) {
+						if (c.getClassTypes2().containsKey(pattern)) {
+							check = new JCheckBox(c.getName());
+							check.setBorder(new EmptyBorder(0, 20, 0, 0));
+							patternPanel.add(check);
+						}
+					}
+					add(patternPanel);
+				}
+			}
+		}
+		
+		@Override
+		public void paintComponent(Graphics comp) {
+			super.paintComponent(comp);
+		}
 	}
 
 	class Panel extends JPanel {
